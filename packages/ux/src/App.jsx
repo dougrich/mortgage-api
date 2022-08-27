c80341b5dc7cc06e0ad17daac2b3d97aa916c11e
Douglas Richardson <dougrich@protonmail.com>
2022-08-27 16:12:34 -0700

    feat(ux): scaffolding for UX

---
+++ packages/ux/src/App.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+const App = () => (
+  <div>Placeholder</div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(<App />)
---
+++ packages/ux/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/App.jsx',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ],
+  devServer: {
+    port: 3030
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
